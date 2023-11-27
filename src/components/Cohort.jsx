635550f0d0f70eaaa3bb610f484340e45246b497
2023temitope <topbeemak@yahoo.com>
2023-11-27 16:16:22 +0000

    savinglloydsreact

---
+++ src/components/Cohort.jsx
@@ -0,0 +1,29 @@
+//components name MUST be capitalised
+function Cohort (props) {
+    console.log("props:", props.props);
+    console.log("Name:", props.name);
+    console.log("Age:", props.age);
+    console.log("Job:", props.job);
+    console.log("height:", props.height);
+    console.log("weight:", props.weight);
+    console.log("kid:", props.kid);
+   
+    return(
+    
+    
+
+<div>
+<p>Name1:(props.Peace)</p>
+<p>Name2:(props.Joy)</p>
+<p>Name3:(props.Love)</p>
+<p>Name:(props.Peaceful)</p>
+<p>Name5:(props.Joyful)</p>
+<p>Name6:(props.Lovely)</p>
+</div>
+);
+
+
+   
+}
+    //lets you import the functio
+    export default Cohort;
